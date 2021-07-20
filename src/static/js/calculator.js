67bd5456141f4f70f7ee93dc7f3ab18ffcbaa32f
Lisandro Bonino <lisandrobonino4@protonmail.com>
2021-07-20 15:36:34 -0300

    Make keys populate the calculator display on click

--- src/static/js/calculator.js
+++ src/static/js/calculator.js
@@ -28,3 +28,28 @@ function operate(operand1, operand2, operator) {
             throw "Invalid operator";
     }
 }
+
+function getWidthPercentage(element) {
+    return (element.offsetWidth * 100) / element.parentNode.offsetWidth;
+}
+
+function populateDisplay() {
+    switch (true) {
+        case (getWidthPercentage(displayValue) > 89):
+        case (displayValue.textContent.includes(".") && this.textContent === "."):
+            return;
+        case (displayValue.textContent === "0" && this.textContent === "."):
+            return displayValue.textContent += this.textContent;
+        case (displayValue.textContent === "0"):
+            return displayValue.textContent = this.textContent;
+        default:
+            return displayValue.textContent += this.textContent;
+    }
+}
+
+const displayValue = document.querySelector("#display__value");
+const altersDisplayButtons = document.querySelectorAll(".key--alters-display");
+
+altersDisplayButtons.forEach(button => {
+    button.addEventListener("click", populateDisplay);
+});  
